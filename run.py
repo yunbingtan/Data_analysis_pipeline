258362701d4d5c98746f21fe8aaf34ada32a6c70
Yunbing Tan <114122258+yunbingtan@users.noreply.github.com>
2026-03-06 14:28:57 -0500

    test ci/cd

--- run.py
+++ run.py
@@ -3,8 +3,8 @@ import pandas as pd
 def main():
     df = pd.read_csv('sales_data_sample.csv', encoding='cp1252')
     df['ORDERDATE'] = pd.to_datetime(df['ORDERDATE'])
-    total_sales = df[df['ORDERDATE'] > '2004-01-01']['SALES'].sum()
-    print(f'Total sales after January 1, 2004: {total_sales}')
+    total_sales = df[df['ORDERDATE'] > '2004-01-02']['SALES'].sum()
+    print(f'Total sales after January 2, 2004: {total_sales}')
     return total_sales
 
 if __name__ == '__main__':
